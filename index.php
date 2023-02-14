3f2716edc2ab9094930622646e7a99b1065e429f
qwseops <40327168+qwseops@users.noreply.github.com>
2023-02-15 04:27:49 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 	$referer = $_SERVER['HTTP_REFERER'];
 	if($referer == "")
 	{
-		echo "<meta http-equiv=\"refresh\" content=\"0;url=https://greatgym2-3.herokuapp.com/2.php">";	
+		echo "<meta http-equiv=\"refresh\" content=\"0;url=http://greatgym2-3.herokuapp.com/2.php">";	
 	}
 ?>
