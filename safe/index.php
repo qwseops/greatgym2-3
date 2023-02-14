24ec34c41f9b660255e3a45002443158b75db9a7
qwseops <40327168+qwseops@users.noreply.github.com>
2023-02-15 04:35:32 +0500

    Add files via upload

---
+++ safe/index.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html><html><head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+
+<title>Great Gym Active Centro natatorio fitness Marcianise</title>
+<meta name="description" content="Centro natatorio fitness, dotato di due piscine, centro Arrampicata, palestra, aree danza e prepugilistica come boxe e judo, un solarium e spazio ludoteca">
+<meta name="keywords" content="Nuoto, fitness, sala, attrezzi, Arrampicata, danza, judo, boxe, piscina, Marcianise">
+<link rel="alternate" hreflang="it" href="http://www.greatgym.it/">
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/flexslider.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="top">
+	<div class="intTop" style="color:#FFFFFF"><p class="font15 colorbianco "><img src="images/telefono.png" alt class="imgAttDX">Assistenza Clienti   (+39)  <strong>0823.58.10.69</strong></p></div>
+
+<nav><div class="logo"><a href="http://www.greatgym.it/"><img src="images/logo.png" alt="Great Gym Active" class="logoMxx"></a></div>
+	<ul class="clearfix">
+    	<li><a href="http://www.greatgym.it/" class="cliccato">HOME</a></li>
+		<li><a href="chisiamo/">CHI SIAMO</a></li>
+		<!--<li><a href="promo-eventi/" class="cliccato">PROMO / EVENTI</a></li>-->
+        <li><a href="corsi/?disc=Nuoto+e+fitness+in+acqua">CORSI</a></li>
+		<li><a href="team/?disc=Nuoto+e+fitness+in+acqua">TEAM</a></li>
+		<li><a href="immagini/">IMMAGINI</a></li>
+        <li><a href="servizi/">SERVIZI</a></li>
+         
+		<li><a href="contatti" class="nobordeRX">CONTATTI</a></li>
+         
+	</ul>
+		<a href="javascript:void(0);" id="pull">Menu</a>
+</nav>
+</div>
+      
+      <div id="slider">
+      <div class="flexslider">
+      <ul class="slides">
+
+<!--     <li><a href="http://www.greatgym.it/corsi/discipline/?d=CrossFit+GGA"><img src="images/01.png" alt="Lo sport in una nuova dimensione"></a></li>
+      <li><a href="http://www.greatgym.it/corsi/discipline/?d=Grit"><img src="images/02.png" alt="Grit Les Mills"></a></li>--> 
+      <li><a href="http://www.greatgym.it/corsi/?disc=Nuoto+e+fitness+in+acqua"><img src="images/03.png" alt="Nuoto e fitness in acqua"></a></li>
+      <li><a href="http://www.greatgym.it/servizi/palestra/?d=Eventi+e+feste+per+bambini"><img src="images/04.png" alt="Feste per bambini ed eventi - Great Gym Cafe"></a></li>
+      
+      <li><img src="images/covid.jpg" alt="Si riparte in sicurezza"></li>
+     </ul>
+      </div> 
+      </div>
+  
+      
+ <main>
+  <div class="blocco94dos allineamentoright"><p class="color999"><a href="http://www.greatgym.it/">home</a> |</p></div>
+ <div class="blocco94">
+	  <div class="blocco32 magineRight iconMargTB">
+	    <a href="corsi/?disc=Nuoto+e+fitness+in+acqua"><img src="images/nuoto.png" width="66" height="66" class="imgIcon" alt="Nuoto e fitness in acqua"></a>
+        <h2><a href="corsi/?disc=Nuoto+e+fitness+in+acqua">Nuoto e fitness in acqua</a></h2>
+        <p class="color999">Nuoto libero, scuola nuoto adulti e bambini oltre a tutti i vari corsi di fitness in acqua</p>
+	  </div>
+    <div class="blocco32 magineLeft magineRight iconMargTB">
+      <a href="corsi/?disc=Fitness"><img src="images/fitness.png" width="66" height="66" class="imgIcon" alt="Fitness"></a>
+      <h2><a href="corsi/?disc=Fitness">Fitness</a></h2>
+      <p class="color999">Corsi dedicati al benessere che spaziano dalle attività posturali a quelle funzionali organizzate con circuiti sia a corpo libero che con attrezzi.</p>
+    </div>
+    <div class="blocco32 magineLeft iconMargTB"> <a href="corsi/discipline/?d=CrossGGA"><img src="images/crossfit.png" width="66" height="66" class="imgIcon" alt="CrossGGA"></a>
+      <h2><a href="corsi/discipline/?d=CrossGGA">CrossGGA</a></h2>
+      <p class="color999">CrossGGA è un costantemente vario, alta intensità, movimenti funzionali</p>
+    </div>
+    <div class="blocco32 magineRight iconMargTB" style="clear:both;"> <a href="corsi/discipline/?d=Sala+attrezzi"><img src="images/salaattrezzi.png" width="66" class="imgIcon" height="66" alt="Sala+attrezzi"></a>
+      <h2><a href="corsi/discipline/?d=Sala+attrezzi">Sala attrezzi</a></h2>
+      <p class="color999">Una grande sala attrezzi vi aspetta, con istruttori e personal training qualificati.</p>
+    </div>
+    <div class="blocco32 magineLeft magineRight iconMargTB"> <a href="corsi/?disc=Arrampicata"><img src="images/freeclimbing.png" width="66" class="imgIcon" height="66" alt="Arrampicata"></a>
+      <h2><a href="corsi/?disc=Arrampicata">Arrampicata</a></h2>
+      <p class="color999">Struttura artificiale d'arrampicata composta da 2 torri con corde e zona boulder</p>
+    </div>
+    <div class="blocco32 magineLeft iconMargTB"> <a href="corsi/?disc=Prepugilistica"><img src="images/combat.png" width="66" height="66" class="imgIcon" alt="Prepugilistica"></a>
+      <h2><a href="corsi/?disc=Prepugilistica">Prepugilistica</a></h2>
+      <p class="color999">Sport da prepugilisticaimento amatoriale e professionale per qualunque età</p>
+    </div>
+ </div>
+    
+ <div class="blocco94dos">
+   <h2>Oggi, 15 febbraio 2023  in Great Gym Active:</h2>
+ </div>
+ <div class="nero100">
+   <div class="blocco94tris">
+     
+     
+     
+     
+     <div class="blocco32 magineRight2">
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Corsi+Nuoto+Libero">Corsi Nuoto Libero</a><div class="floatRX">08:00 - 11:00</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Acqua+Gym">Acqua Gym</a><div class="floatRX">09:00 - 09:45</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=Postural+Pilates">Postural Pilates</a><div class="floatRX">10:00 - 11:00</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=Group+Cycling">Group Cycling</a><div class="floatRX">13:30 - 14:30</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=CrossGGA">CrossGGA</a><div class="floatRX">13:30 - 14:30</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=AQUA+GAG">AQUA GAG</a><div class="floatRX">14:00 - 14:45</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Scuola+Nuoto+Bambini+(4-15+anni)">Scuola Nuoto Bambini</a><div class="floatRX">16:50 - 17:40</div></div>
+       
+      </div>
+     
+     <div class="blocco32 magineLeft2 magineRight2">
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Scuola+Nuoto+Bambini+(4-15+anni)">Scuola Nuoto Bambini</a><div class="floatRX">17:30 - 18:20</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=Postural+Pilates">Postural Pilates</a><div class="floatRX">18:00 - 19:00</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Scuola+Nuoto+Bambini+(4-15+anni)">Scuola Nuoto Bambini</a><div class="floatRX">18:10 - 19:00</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=CrossGGA">CrossGGA</a><div class="floatRX">18:30 - 19:30</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=TRX">TRX</a><div class="floatRX">19:00 - 20:00</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=Total+body">Total body</a><div class="floatRX">19:00 - 20:00</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=AQUA+GAG">AQUA GAG</a><div class="floatRX">19:00 - 19:45</div></div>
+       
+      </div>
+     <div class="blocco32 magineLeft2">
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Acqua+GAG">Acqua GAG</a><div class="floatRX">19:00 - 19:45</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=Prepugilistica">Prepugilistica</a><div class="floatRX">19:30 - 21:00</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=CrossGGA">CrossGGA</a><div class="floatRX">19:30 - 20:30</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Scuola+Nuoto+Adulti">Scuola Nuoto Adulti</a><div class="floatRX">19:40 - 20:30</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Acqua+Gym">Acqua Gym</a><div class="floatRX">19:45 - 20:30</div></div>
+       
+       <div class="intProgrAzz"><a href="corsi/discipline/?d=Scuola+Nuoto+Adulti">Scuola Nuoto Adulti</a><div class="floatRX">20:25 - 21:15</div></div>
+       
+       <div class="intProgr"><a href="corsi/discipline/?d=Arrampicata+Libera+Assistita">Arrampicata Libera Assistita</a><div class="floatRX">20:30 - 22:30</div></div>
+       
+      </div>
+     
+    </div>
+ </div>
+ 
+ <div class="blocco94dos allineamentoright1">
+   <p class="color999"><a href="public/griglia-corsi.pdf" target="_blank"><img src="images/scarica.png" width="22" height="22" alt="scarica" class="imgAttDX">scarica il programma completo</a></p>
+ </div>
+ 
+ </main>
+      
+
+<div class="blocco94">
+   
+
+	  <div class="blocco30 magineLeft magineRight marginBottom2 altTEAM"> 
+      
+        <a href="servizi/palestra/?d=Eventi+e+feste+per+bambini"><img src="images/feste_a.jpg" class="logoMax" alt></a>
+      
+	    <div class="blocco100">
+	      <h2><a href="servizi/palestra/?d=Eventi+e+feste+per+bambini">Servizi - Eventi e feste per bambini</a></h2>
+	      <p class="color999">La Great Gym Active ha un settore dedicato alle feste ed eventi</p>
+        </div>
+     </div>
+   
+	  <div class="blocco30 magineLeft magineRight marginBottom2 altTEAM">
+      
+        <a href="corsi/discipline/?d=Acqua+step"><img src="images/acquastep_a.jpg" class="logoMax" alt></a>
+      
+
+	    <div class="blocco100">
+	      <h2><a href="corsi/discipline/?d=Acqua+step">Corso - Acqua step</a></h2>
+          <p class="color999"></p><p><strong>2 lezioni a settimana</strong><br> Martedì e Giovedì 14:00<br> <br> <strong>3 lezioni a settimana</strong><br> Lunedì, Mercoledì e Venerdì 20:30</p><p></p>
+        </div>
+     </div>
+
+	  <div class="blocco30 magineLeft magineRight marginBottom2 altTEAM"> 
+      
+      <a href="team/?disc=Nuoto+e+fitness+in+acqua;"><img src="images/avagnano.jpg" class="logoMax" alt></a>
+      
+	    <div class="blocco100">
+	      <h2><a href="team/?disc=Nuoto+e+fitness+in+acqua;">Team - Gianmarco Avagnano</a></h2>
+	      <p class="color999">Laureato in Fisioterapia; Responsabile della piscina; Direttore sportivo e gestore di impianti (FIN); istruttore base  e maestro di salvamento (FIN). Fiduciario locale salvamento FIN</p>
+        </div>
+     </div>
+ 
+</div>
+
+<div class="blocco94dos sfondoGrigio">
+	<div class="blocco32 magineRight"> <p class="font17 colorbianco lineH36">Follow us<img src="images/social.png" alt usemap="#Map" class="imgAttSX">
+        <map name="Map">
+          <area shape="rect" coords="2,2,35,34" href="https://www.facebook.com/GreatGymActive/" target="_blank">
+          <area shape="rect" coords="37,2,70,34" href="https://plus.google.com/108021878178223914851/posts" target="_blank">
+          <area shape="rect" coords="72,2,105,34" href="https://www.instagram.com/great_gym_active/" target="_blank">
+          <area shape="rect" coords="107,2,140,34" href="http://greatgymactive.tumblr.com/" target="_blank">
+          <area shape="rect" coords="142,2,175,34" href="https://twitter.com/great_gym" target="_blank">
+          <area shape="rect" coords="177,2,210,34" href="https://www.youtube.com/channel/UCOgh2PSA0PosMgX62vby5lA" target="_blank">
+        </map>
+      </p></div>
+    <div class="blocco32 magineLeft magineRight"><p class="font17 colorbianco ">WhatsApp:<br>
+      <strong>piscina 345.57.76.422 | Palestra 345.43.32.313</strong></p>
+    </div>
+    <div class="blocco32 magineLeft">
+      <p class="font17 colorbianco "><img src="images/telefono.png" alt class="imgAttDX">Assistenza Clienti   (+39)  <strong>0823.58.10.69</strong></p>
+    </div>
+</div>
+<footer>
+	<div class="blocco94dos">
+	  <div class="blocco32 magineRight">
+        <p>Great Gym Active<br>
+          S.P. 336 (ex S.S. 87), Km. 20600<br>
+          81025 - Marcianise (Ce) Italia<br>
+          Uscita A1 Caserta Sud - direzione Caserta<br>
+  <br>
+  <br>
+          tel: (+39) 0823.58.10.69 | 0823.82.15.32<br>
+          mail: info@greatgym.it<br>
+  <br>
+  <br>
+        <!--Regole Club  |  --><a href="privacy/">Privacy</a> | <a href="cookies/">Cookie Policy</a> </p>
+	  </div>
+	  <div class="blocco32 magineLeft magineRight">
+      <p>Affiliato:</p>
+      <img src="images/coni_fin.png" alt><br><br>
+
+        <p>Orari di apertura:<br>
+          Lunedì - Venerdì: 06:30 > 22:30<br>
+          Sabato: 08:00 > 17:00<br>
+          Domenica: 09:00 > 12:30</p>
+	  </div>
+	  <div class="blocco32 magineLeft">
+        <p>Iscritta al Registro delle Imprese di Napoli REA<br>
+          n. NA278296 - P.IVA 00283670636<br>
+          Soggetta a direzione e coordinamento di <br>
+          C.P. Cementi Prefabbricati srl<br>
+          Sede legale: C.so Umberto I - 55  80138 Napoli<br>
+          <br>
+          © Copyright 2023 Great Gym Active. <br>
+          All rights reserved<br>
+          <br>
+          Credits: <a href="http://www.gianniorlando.com" target="_blank" rel="nofollow">Gianni Orlando</a></p>
+	  </div>
+    
+    </div>
+</footer>
+
+
+
+<script>var root = "";</script>
+<script defer src="js/todo.js"></script>
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script async src="js/selectivizr-min.js"></script>
+<![endif]--></body></html>
